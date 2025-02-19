e81f5adfd315c08cbca309dc4b196e0c50d6b5b8
kev0000exe <126819655+kev0000exe@users.noreply.github.com>
2025-02-19 16:25:56 -0600

    explicacion md

---
+++ explicacion.md
@@ -0,0 +1,7 @@
+buenos dias ing aqui explico brevemente de como ocuparla
+
+entra a backend.cs
+
+Edita appsettings.json y agregue su base de datos  despues aplicar las migraciones 
+
+ya seria de ejecutar el proyecto ya teniendo su base de datos y en el video explico como funciona que pase feliz dia 
--- obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3835e5dc7dcaabe225f99d3b7c18aaf5a02cdc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
